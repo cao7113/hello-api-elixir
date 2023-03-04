d63e8363bd1d723c065d11585e871e0abc7c5eff
Ruijian Cao <caoruijian@big.one>
2023-03-04 21:40:39 +0800

    fix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule HelloApi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        hello_api: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent],
+          steps: [:assemble, :tar]
+        ]
+      ]
     ]
   end
 
