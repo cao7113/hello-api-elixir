7e895b34ce4a2d8481b6a7844c797faf88611115
Ruijian Cao <caoruijian@big.one>
2023-03-12 22:36:17 +0800

    Bump version to 0.1.27

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HelloApi.MixProject do
   def project do
     [
       app: :hello_api,
-      version: "0.1.26",
+      version: "0.1.27",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
